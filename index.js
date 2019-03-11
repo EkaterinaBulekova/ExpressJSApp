acb10e84b786d4d15997a707a6f3af61d69b09d1
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2019-03-11 11:12:08 +0600

    Added new Hello world message with react

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var ReactDOMServer = require('react-dom/server');
+var react = require('react');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("<!DOCTYPE html><html><head><title>My Page</title></head><body><div id='content'> "+
+  ReactDOMServer.renderToString(react.createElement('div', null, 'Hello World'))+
+  " </div></body></html>");
+
 });
 
 app.listen(3000, function () {
